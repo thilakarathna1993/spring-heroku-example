9251ec1a1a37c4971818a549595f2d9b434539c2
thilakarathna1993 <51091161+thilakarathna1993@users.noreply.github.com>
2019-05-28 22:14:56 +0530

    Update SampleController.java

--- src/main/java/com/pasindu/spring/springherokuexample/controller/SampleController.java
+++ src/main/java/com/pasindu/spring/springherokuexample/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.pasindu.spring.springherokuexample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
