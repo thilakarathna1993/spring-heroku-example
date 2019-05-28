ee6e8238885937c28ac3ee6aa609bc3bf4ffaf23
thilakarathna1993 <51091161+thilakarathna1993@users.noreply.github.com>
2019-05-28 21:54:07 +0530

    Add files via upload

---
+++ src/main/java/com/pasindu/spring/springherokuexample/SpringHerokuExampleApplication.java
@@ -0,0 +1,13 @@
+package com.pasindu.spring.springherokuexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHerokuExampleApplication.class, args);
+    }
+
+}
